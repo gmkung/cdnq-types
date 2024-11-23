59955dd2eebc0538cf77fbbfb5c496a815c0f9a5
gmkung <guangmian@gmail.com>
2024-11-23 11:08:44 +0800

    Remove node_modules and dist from Git tracking

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface CDNEntry {
-    "Contract address": string;
-    "Domain name": string;
-    "Visual proof": string;
-}
-export interface ICDNService {
-    returnTags(chainId: string, apiKey: string | null): Promise<CDNEntry[]>;
-}
