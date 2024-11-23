fca9aeff8ed4d8ebb3301e7a5d4c622dc903e03e
gmkung <guangmian@gmail.com>
2024-11-23 11:05:02 +0800

    Initial commmit

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export interface CDNEntry {
+    "Contract address": string;
+    "Domain name": string;
+    "Visual proof": string;
+}
+export interface ICDNService {
+    returnTags(chainId: string, apiKey: string | null): Promise<CDNEntry[]>;
+}
